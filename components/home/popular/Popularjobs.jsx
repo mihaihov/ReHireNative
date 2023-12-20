95e0bd17b716840a6e4e6404e32d3cc4e9b10cc0
mihairaducu <raducumihaicristian@gmail.com>
2023-12-20 02:32:58 +0200

    Creating the scheleton of the app. Building the Welcome scree

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, Text,TouchableOpacity,FlatList,ActivityIndicator } from 'react-native';
+import { useState } from 'react';
+import { useRouter } from 'expo-router';
+
+import styles from './popularjobs.style';
+import {COLORS,SIZES} from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+
+const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  const error = false
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size = "large" colors={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ): (
+          <FlatList
+            data = {[1,2,3,4]}
+            renderItem={({item}) => (
+              <PopularJobCard
+                item = {item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle = {{columnGap: SIZES.medium}}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default Popularjobs
